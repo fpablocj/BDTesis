d3388c87619a84423fd4420abbaa14e2f44eaff5
Pablo Faican <pabloxd.2010@gmail.com>
2023-09-04 14:27:47 -0500

    Corrección de error de prospectos ya existe

--- src/controller/ProspectoController.ts
+++ src/controller/ProspectoController.ts
@@ -218,7 +218,7 @@ static getCountByCarreraAndEstado = async (req: Request, res: Response) => {
       //prospecto.hashPassword();
       await prospectoRepository.save(prospecto);
     } catch (e) {
-      return res.status(409).json({ message: 'Prospecto already exist', e });
+      return res.send({ message: 'Prospecto already exist', e });
     }
     // All ok
     res.send({ message: 'Prospecto created' });
